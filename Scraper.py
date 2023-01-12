98d5cdc8fff046064f18a8b725e96bf1d46feab0
Scarlet <66797411+TanZeus@users.noreply.github.com>
2023-01-12 20:48:27 +0530

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -11,7 +11,7 @@ tree = BeautifulSoup(r.content, 'html.parser')
 good_html = tree.prettify()
 for hd in tree.find_all("h1"):
     hde=hd.text
-    print("HEADD - ", hde, "\n")
+    print("HEAD - ", hde, "\n")
 for ra in tree.find_all("p"):
     para1=ra.text
     para+=para1
@@ -23,3 +23,6 @@ f=open("Para_Swap.txt", "w")
 f.write("Title\n")
 f.write(concat_para)
 f.close()
+
+df = pd.read_csv('Para_Swap.txt', sep='\t')
+df.to_csv('output.csv', index=False)
