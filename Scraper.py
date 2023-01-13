7864f646d0210c20b2e2098397fb7d19121eaa3a
Scarlet <bhandwalkartanish513@gmail.com>
2023-01-13 20:32:41 +0530

    modifying Scraper to work externally

--- Scraper.py
+++ Scraper.py
@@ -7,17 +7,23 @@ import os
 import pandas as pd
 from googlesearch import search
 
-
 res_url=[]
 para=""
 que = input("Enter Keyword to find : ")
+
 def goosearch(query):
     query += " site:news.google.com"
     sleep(3)
     qser = search(query, num_results=10)
     for url in qser:
         res_url.append(url)
-        
+
+def fwrite(cpa):
+    f=io.open("Para_Swap.txt", "w", encoding="UTF-8")
+    f.write("Title\n")
+    f.write(cpa)
+    f.close()
+
 goosearch(que)
 for i in range(len(res_url)):
     x=res_url[i]
@@ -26,16 +32,11 @@ for i in range(len(res_url)):
     good_html = tree.prettify()
     for hd in tree.find_all("h1"):
         hde=hd.text
-        print("HEADD - ", hde, "\n")
+        print("HEAD - ", hde, "\n")
     for ra in tree.find_all("p"):
         para1=ra.text
         para+=para1
 
 concat_para=para.replace('.','.\n ')
 print(concat_para)
-
-f=io.open("Para_Swap.txt", "w", encoding="UTF-8")
-f.write("Title\n")
-f.write(concat_para)
-f.close()
 
