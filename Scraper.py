859d436320acada3669d44ff5148d0e1c5de9946
Scarlet <bhandwalkartanish513@gmail.com>
2023-01-13 20:16:44 +0530

    Updated Scraper to usable version.
    This version can now fetch data using a keyword and store the content to Para_Swap.txt to be used as a nlp dataset.

--- Scraper.py
+++ Scraper.py
@@ -1,3 +1,5 @@
+from time import sleep
+import io
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -7,31 +9,33 @@ from googlesearch import search
 
 
 res_url=[]
+para=""
 que = input("Enter Keyword to find : ")
 def goosearch(query):
-  query += " site:news.google.com"
-  for url in search(query):
-    res_url.append(url)
-
-x=input("Enter URL : ")
-r=requests.get(x)
-para=""
-tree = BeautifulSoup(r.content, 'html.parser')
-good_html = tree.prettify()
-for hd in tree.find_all("h1"):
-    hde=hd.text
-    print("HEAD - ", hde, "\n")
-for ra in tree.find_all("p"):
-    para1=ra.text
-    para+=para1
+    query += " site:news.google.com"
+    sleep(3)
+    qser = search(query, num_results=10)
+    for url in qser:
+        res_url.append(url)
+        
+goosearch(que)
+for i in range(len(res_url)):
+    x=res_url[i]
+    r=requests.get(x)
+    tree = BeautifulSoup(r.content, 'html.parser')
+    good_html = tree.prettify()
+    for hd in tree.find_all("h1"):
+        hde=hd.text
+        print("HEADD - ", hde, "\n")
+    for ra in tree.find_all("p"):
+        para1=ra.text
+        para+=para1
 
 concat_para=para.replace('.','.\n ')
 print(concat_para)
 
-f=open("Para_Swap.txt", "w")
+f=io.open("Para_Swap.txt", "w", encoding="UTF-8")
 f.write("Title\n")
 f.write(concat_para)
 f.close()
 
-df = pd.read_csv('Para_Swap.txt', sep='\t')
-df.to_csv('output.csv', index=False)
